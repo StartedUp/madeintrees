8b668986dc8f9fe175377ab22ca66a1ba0ab399e
Prithviprakash <prithviprakash.r@cortexcraft.com>
2017-03-21 21:03:37 +0530

    Starter from spring

---
+++ src/main/java/com/madeintrees/MadeintreesApplication.java
@@ -0,0 +1,12 @@
+package com.madeintrees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadeintreesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadeintreesApplication.class, args);
+	}
+}
